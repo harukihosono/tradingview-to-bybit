7610dad5fc4884ed8b854e9f362daf60be3fe6ea
harukihosono <73732833+harukihosono@users.noreply.github.com>
2025-01-03 14:56:49 +0900

    Update app.py
    port 80へ変更

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def trade():
         return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
